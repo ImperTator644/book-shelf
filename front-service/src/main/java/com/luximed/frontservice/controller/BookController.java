2906a172ade4dcf157b9edd7fccdc3dbbe7bab67
ImperTator644 <ukaczyski6@gmail.com>
2023-08-15 00:59:24 +0200

    feature/book-overview-template Created book overview template with image and title

---
+++ front-service/src/main/java/com/luximed/frontservice/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.luximed.frontservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/book")
+public class BookController {
+    @GetMapping
+    public String getBookOverviewPage() {
+        return "books/book-overview";
+    }
+}
