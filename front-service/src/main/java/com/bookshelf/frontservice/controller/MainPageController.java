3b8a2939aad3036daf765b56a8ca4b5349966445
ImperTator644 <ukaczyski6@gmail.com>
2024-01-03 21:38:53 +0100

    feature/feature Main page basic

--- front-service/src/main/java/com/bookshelf/frontservice/controller/MainPageController.java
+++ front-service/src/main/java/com/bookshelf/frontservice/controller/MainPageController.java
@@ -1,12 +1,23 @@
 package com.bookshelf.frontservice.controller;
 
+import com.bookshelf.frontservice.service.CurrentUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainPageController {
+
+    private final CurrentUserService currentUserService;
+
     @GetMapping(value = "/")
-    public String getMainPage() {
+    public String getMainPage(ModelMap modelMap) {
+        if(currentUserService.getCurrentUser().equals(currentUserService.getEmptyUser())) {
+            return "main-template";
+        }
+        modelMap.put("currentUser", currentUserService.getCurrentUser());
         return "main-template";
     }
 }
